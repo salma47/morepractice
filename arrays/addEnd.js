fabb1bb2eea7ad0bad2107d2736fdd28b58291df
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2023-01-19 23:12:41 +0100

    pushFunction

--- arrays/addEnd.js
+++ arrays/addEnd.js
@@ -1,12 +1,13 @@
 // Create a function called addEnd that takes in an array and a number and
 // positions the number at the end of the array. Return the new array.
 
-var arr=[1,71,2,96];
-var num;
+// var arr=[1,71,2,96];
+// var num;
 function addEnd(arr,num){
-    add = arr.append(num);
-    return add
+    add = arr.push(num);
+    return arr
 }
+console.log(addEnd([5,5,6],2));
 
 
 // DO NOT DELETE
