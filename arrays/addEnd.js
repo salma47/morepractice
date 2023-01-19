833d600c313e79227ba872a0865291f75c231b5b
Salma-Hamzaoui <hamzaoui.salma@gmail.com>
2023-01-19 23:07:08 +0100

    firstStep

--- arrays/addEnd.js
+++ arrays/addEnd.js
@@ -1,7 +1,13 @@
 // Create a function called addEnd that takes in an array and a number and
 // positions the number at the end of the array. Return the new array.
 
-var addEnd;
+var arr=[1,71,2,96];
+var num;
+function addEnd(arr,num){
+    add = arr.append(num);
+    return add
+}
+
 
 // DO NOT DELETE
 module.exports = addEnd;
